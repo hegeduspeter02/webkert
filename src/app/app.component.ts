e5bb18122346c754d8ee5876e7796fbf7175fd19
Hegedus Peter Laszlo <h158522@stud.u-szeged.hu>
2024-04-16 09:04:40 +0200

    handsontables initialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HotTableModule} from "@handsontable/angular";
+import {registerAllModules} from "handsontable/registry";
+
+// register Handsontable's modules
+registerAllModules();
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HotTableModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'webkert-projekt';
+
 }
