c12ca0b35b7e89b5e3e306cd01868436292b6217
Hegedus Peter Laszlo <h158522@stud.u-szeged.hu>
2024-04-20 23:35:46 +0200

    delete unused

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,9 +4,6 @@ import {MatToolbar, MatToolbarRow} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatSidenav, MatSidenavContainer} from "@angular/material/sidenav";
-import {map, Observable, shareReplay} from "rxjs";
-import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
-import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
@@ -15,12 +12,7 @@ import {AsyncPipe} from "@angular/common";
     RouterLink,
     RouterLinkActive,
     MatToolbar,
-    MatIcon,
-    MatIconButton,
     MatToolbarRow,
-    MatButton,
-    MatSidenavContainer,
-    MatSidenav
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
